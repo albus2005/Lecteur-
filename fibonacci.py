f06f2931770db75f42d1386ff0a332bd2af35b76
albus2005 <aristotelu2005@gmail.com>
2026-04-26 18:35:03 +0200

    Create fibonacci.py
    je viens de coder la saisie du nombre pour ce premier commit

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+print("--- FIBONACCI ---")
+while True:
+    try: 
+        number = int(input("Entrez un nombre de votre choix: ")
+        break
+    
+    except ValueError:
+        print("Entrez un nombre entier !")
+
